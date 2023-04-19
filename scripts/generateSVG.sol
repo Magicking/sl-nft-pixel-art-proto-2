91a23321053aaf167a56a8595fc27b0c5b096335
Sylvain Laurent <s@6120.eu>
2023-04-19 02:19:18 +0200

    Fix display on latest lilnouns by directly accessing private data

--- scripts/generateSVG.sol
+++ scripts/generateSVG.sol
@@ -14,16 +14,22 @@ contract generateSVG is Script {
         //TODO: create a function that call the NounsToken 0x9C8fF314C9Bc7F6e59A9d9225Fb22946427eDC03 to obtain the seed generator address
         // and then call the INounsSeeder to obtain the seed
         // and then call the INounsDescriptor to obtain the tokenURI
-        // and then call the INounsDescriptor to obtain the dataURI
-        // and then call the INounsDescriptor to obtain the genericDataURI
-        // and then call the INounsDescriptor to obtain the generateSVGImage
         //address nounsTokenAddress = 0x9C8fF314C9Bc7F6e59A9d9225Fb22946427eDC03; // Nouns
-        address nounsTokenAddress = 0x4b10701Bfd7BFEdc47d50562b76b436fbB5BdB3B; // LilNous
+        address nounsTokenAddress = 0x4b10701Bfd7BFEdc47d50562b76b436fbB5BdB3B; // LilNouns
         INounsToken nounsToken = INounsToken(nounsTokenAddress);
         //INounsSeeder.Seed memory seed = nounsToken.seeder().generateSeed(tokenId, nounsToken.descriptor());
-        //tokenId = nounsToken.totalSupply();
+        INounsSeeder.Seed memory seed;
+        // Iterate backwards through all the tokens
+        for (;tokenId > 0;tokenId--) {
+            seed = nounsToken.seeds(tokenId);
+            if (seed.background != 0 ||
+                seed.body != 0 ||
+                seed.accessory != 0 ||
+                seed.head != 0 ||
+                seed.glasses != 0
+                ) break;
+        }
         console.log("tokenId:", tokenId);
-        INounsSeeder.Seed memory seed = nounsToken.seeds(tokenId);
         uint48 h = uint48(uint256(keccak256(abi.encodePacked(blockhash(block.number - 1), tokenId))));
         uint48 gCount = uint48(nounsToken.descriptor().glassesCount());
         seed.glasses = (seed.glasses+h) % gCount;
@@ -31,17 +37,12 @@ contract generateSVG is Script {
     }
 
     function run() external {
+        address nounsTokenAddress = 0x4b10701Bfd7BFEdc47d50562b76b436fbB5BdB3B; // LilNouns
+        bytes32 CURRENT_INTERNAL_ID_SLOT = bytes32(0x0000000000000000000000000000000000000000000000000000000000000015);
+        uint256 currentInternalId = uint256(vm.load(address(nounsTokenAddress), CURRENT_INTERNAL_ID_SLOT));
 
-      // usurpate / prank to minter address
-        address nounsTokenAddress = 0x4b10701Bfd7BFEdc47d50562b76b436fbB5BdB3B; // LilNous
-        INounsToken nounsToken = INounsToken(nounsTokenAddress);
-      address minter = nounsToken.minter();
-      vm.prank(minter);
-      // call mint() to return currentNoudId
-      uint256 currentInternalId=nounsToken.mint();
-      // usurpate and set tokenid
-      console.log("expected internal id", currentInternalId);
-        (string memory uriS, INounsSeeder.Seed memory seed) = tokenURI2(7621);
+        console.log("expected internal id", currentInternalId);
+        (string memory uriS, INounsSeeder.Seed memory seed) = tokenURI2(currentInternalId);
         
         bytes memory uri = bytes(uriS);
         vm.writeFileBinary("./image.b64uri", uri);
